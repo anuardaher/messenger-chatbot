bea074e41a9141ae4f17ff1c9a318df524aa386c
Anuar <anuar_daher@hotmail.com>
2019-03-19 18:34:24 -0300

    .

--- src/process-message.js
+++ src/process-message.js
@@ -26,22 +26,23 @@ const { FACEBOOK_ACCESS_TOKEN } = process.env;
 const sendTextMessage = (userId, text) => {
     console.log(`Enviando mensagem de volta: ${text}`);
     return fetch(
-        `https://graph.facebook.com/v2.6/me/messages?access_token=${FACEBOOK_ACCESS_TOKEN}`, {
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            method: 'POST',
-            body: JSON.stringify({
-                messaging_type: 'RESPONSE',
-                recipient: {
-                    id: userId,
+            `https://graph.facebook.com/v2.6/me/messages?access_token=${FACEBOOK_ACCESS_TOKEN}`, {
+                headers: {
+                    'Content-Type': 'application/json',
                 },
-                message: {
-                    text,
-                },
-            }),
-        }
-    ).catch(err => console.error(err));;
+                method: 'POST',
+                body: JSON.stringify({
+                    messaging_type: 'RESPONSE',
+                    recipient: {
+                        id: userId,
+                    },
+                    message: {
+                        text,
+                    },
+                }),
+            }
+        ).then(res => console.log(res))
+        .catch(err => console.error(err));
 }
 
 module.exports = (event) => {
