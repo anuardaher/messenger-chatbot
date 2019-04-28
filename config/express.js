2a0691f8f9d6f90c9204b66dbc1d03fb9a371117
Anuar <anuar_daher@hotmail.com>
2019-04-28 12:17:59 -0300

    .

--- config/express.js
+++ config/express.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const messageWebhook = require('../src/message-webhook');
 require("../config/database")("mongodb://admin:z8gr922g@ds021172.mlab.com:21172/aparecida-chatbot")
 
 const app = express()
 
 app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.static(__dirname, { dotfiles: 'allow'}))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname || process.env.NODE_ENV, { dotfiles: 'allow' }))
 
 app.get('/', (req, res) => {
     res.status(200).send("Olá! Sou um chatbot!")
