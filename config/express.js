31e886e5081d97deaefbfcbce65b1e63f9428bfd
root <root@localhost.localdomain>
2019-04-28 03:29:03 -0400

    .

--- config/express.js
+++ config/express.js
@@ -3,10 +3,11 @@ const bodyParser = require('body-parser');
 const messageWebhook = require('../src/message-webhook');
 require("../config/database")("mongodb://admin:z8gr922g@ds021172.mlab.com:21172/aparecida-chatbot")
 
-const app = express();
+const app = express()
 
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(express.static(__dirname, { dotfiles: 'allow'}))
 
 app.get('/', (req, res) => {
     res.status(200).send("Olá! Sou um chatbot!")
