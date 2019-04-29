9bb575abf5e130df9f334176a776b4e10c97616e
Anuar <anuar_daher@hotmail.com>
2019-04-29 18:07:15 -0300

    landing page

--- config/express.js
+++ config/express.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const messageWebhook = require('../src/message-webhook');
+const path = require('path')
 require("../config/database")("mongodb://admin:z8gr922g@ds021172.mlab.com:21172/aparecida-chatbot")
 
 const app = express()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname, { dotfiles: 'allow' }))
 
 app.get('/aparecidabot', (req, res) => {
-    res.status(200).send("Olá! Sou um chatbot!")
+    res.sendFile(path.join(__dirname, '../src/views/index.html'));
 })
 app.post('/aparecidabot/webhook', messageWebhook);
 
